ceb9c986029ad2ec8219a6d4c93bc7b3af5fe15c
Peterhabit <jiseokchoij@gmail.com>
2022-04-18 02:21:30 +0900

    "CS50_2"

---
+++ mario1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+    do{
+    n= get_int("size : ");
+    }
+    while(n<1);
+     for(int i=0; i<n; i++)
+    {
+         printf("\n");
+        for(int j=0; j<n; j++)
+        {
+        printf("#");
+        }
+
+    }
+     printf("\n");
+    
+
+}
+
